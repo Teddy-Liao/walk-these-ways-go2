1219b76e96f363250b9899cb5d189020cf740aa7
Liao Dengting <540614105@qq.com>
2024-03-07 15:42:23 +0800

    delete pycache folder

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-    name='go1_gym',
+    name='go2_gym',
     version='1.0.0',
-    author='Gabriel Margolis',
+    author='Dengting Liao',
     license="BSD-3-Clause",
     packages=find_packages(),
-    author_email='gmargo@mit.edu',
-    description='Toolkit for deployment of sim-to-real RL on the Unitree Go1.',
+    author_email='540614105@qq.com',
+    description='Toolkit for deployment of sim-to-real RL on the Unitree Go2.',
     install_requires=['ml_logger==0.8.117',
                       'ml_dash==0.3.20',
                       'jaynes>=0.9.2',
